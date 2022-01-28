be677417bd5c944b11596bfbc4937ebac8bc5c33
Talha Y <tycode0@gmail.com>
2022-01-28 23:50:04 +0100

    fixed the bug of selecting ulElement

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -33,6 +33,8 @@ export const initQuestionPage = (userInterface) => {
 
   const answersListElement = document.getElementById(ANSWERS_LIST_ID);
 
+  const answerElements = document.getElementById(ANSWERS_LIST_ID).children;
+
   for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
     const answerElement = createAnswerElement(key, answerText);
     answersListElement.appendChild(answerElement);
@@ -88,9 +90,9 @@ export const initQuestionPage = (userInterface) => {
     .getElementById(SHOW_CORRECT_ANSWER_BUTTON_ID)
     .addEventListener('click', showCorrectAnswer);
 
-  document
-    .getElementById(ANSWERS_LIST_ID)
-    .addEventListener('click', selectAnswer);
+  [...answerElements].forEach((answerElement) =>
+    answerElement.addEventListener('click', selectAnswer)
+  );
 };
 const nextQuestion = () => {
   quizData.currentQuestionIndex++;
@@ -106,7 +108,10 @@ export const clock = () => {
   tickingSound.play();
   const countDown = () => {
     remainingTime--;
-    if (remainingTime === 0) {
+    if (
+      quizData.currentQuestionIndex === quizData.questions.length ||
+      remainingTime === 0
+    ) {
       tickingSound.pause();
       tickingSound.currentTime = 0;
       bellRingSound.play();
@@ -114,5 +119,5 @@ export const clock = () => {
       router('result');
     }
   };
-  let animation = setInterval(countDown, 1000);
+  const animation = setInterval(countDown, 1000);
 };
